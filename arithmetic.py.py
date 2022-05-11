2105f080ef627740cd9cbc51be444454c3b7df5f
RamilAssad <ramil_tolubaev@mail.ru>
2022-05-11 11:47:56 +0300

    added commits

---
+++ arithmetic.py.py
@@ -0,0 +1,3 @@
+userAnswer = input("Calc this 4*100-54")
+print("Right answer:", 346)
+print("Your answer:", userAnswer)
---
+++ individual.py.py
@@ -0,0 +1,4 @@
+a = int(input('Dlina bolshego osnovaniya:'))
+b = int(input('Dlina menshego osnovaniya:'))
+h = int(input('Vysota:'))
+print('P = ', 0.5*(a+b)*h)
